3bb2a88b32495b912b41f6a730961ba2eb895d1a
Zane Scheel <zane.scheel@gmail.com>
2021-12-11 13:29:12 -0600

    added all the prompts, and started the generation

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordItems = {};
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var symbols = "!@#$%^&*+=?_-"
+var numbers = "1234567890"
 
 function generatePassword() {
-  var passwordLength = prompt("How long do you want the password?");
-  var lowerCase = prompt("Do you want lowercase letters?");
-  var upperCase = prompt("Do you want ippercase letters?");
-  var numbers = prompt("Do you want numbers?");
-  var specialCharacters = prompt("Do you want special characters?");
+  passwordContent();
+  console.log(passwordItems);
+  password = "";
+  characters = "";
+
+  if(passwordItems)
+}
+
+function passwordContent() {
+  passwordItems.characters = prompt("How many characters do you want in the password?");
+  
+  if(passwordItems.characters < 8 || passwordItems.characters === null) {
+    alert("Must be at least 8 characters, try again.")
+    passwordContent();
+  } else {
+    passwordItems.lowerCase = confirm("Do you want lowercase letters?");
+    passwordItems.upperCase = confirm("Do you want uppercase letters?");
+    passwordItems.numbers = confirm("Do you want numbers?");
+    passwordItems.specialCharacters = confirm("Do you want special characters?");
+  }
 }
 
 // Write password to the #password input
